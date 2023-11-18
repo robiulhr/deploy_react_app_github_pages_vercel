37f2837315544dbb3e5b5033310e0c3025d9db6a
robiulhr <rh22012001@gmail.com>
2023-11-18 19:25:39 +0600

    finished both github pages and vercel deployment setup

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/deploy_react_app_github_pages_vercel'
+  base: process.env.VITE_BASE_PATH || '/deploy_react_app_github_pages_vercel',
 })
